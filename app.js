2222846452ade0fe748acedbd481da8700a7bafb
Ming Ying <misspog42@gmail.com>
2021-04-27 21:24:26 -0400

    third commit for port heroku

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ mongoose.connect(process.env.CONNECTION_STRING,{
 
 let port = process.env.PORT || 3000;
  
-app.listen(port,()=>{
+app.listen(port,()=>{ 
  
   console.log("server running);
 });
